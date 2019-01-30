1dc8e30316508dd2cb9e74d28bfae9caa440bde7
DeuklyoungKo <deutschgon@gmail.com>
2019-01-30 17:41:00 +0100

    for chapter 1-4-2

--- app.php
+++ app.php
@@ -2,43 +2,20 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use DiDemo\Mailer\SmtpMailer;
 use \Pimple\Container;
-use \DiDemo\FriendHarvester;
 
 /* START BUILDING CONTAINER */
 $container = new Container();
 
-$container['database.dsn'] =  'sqlite:'.__DIR__.'/data/database.sqlite';
-$container['smpt.server'] = 'server';
-$container['smpt.user'] = 'user';
-$container['smpt.password'] = 'password';
-$container['smpt.port'] = 'port';
-
-$container['mailer'] = function (Container $container) {
-    return new SmtpMailer(
-        $container['smpt.server'],
-        $container['smpt.user'],
-        $container['smpt.password'],
-        $container['smpt.port']
-    );
-};
-
-$container['friend_harvester'] = function (Container $container) {
-    return new FriendHarvester( $container['pdo'],$container['mailer']);
-};
-
-$container['pdo'] = function (Container $container) {
-    return new PDO($container['database.dsn'] );
-};
-
+require __DIR__.'/app/config.php';
+require __DIR__.'/app/services.php';
 
 //$dsn = 'sqlite:'.__DIR__.'/data/database.sqlite';
 //$pdo = new PDO($dsn);
 /* END CONTAINER BUILDING */
 //$mailer = new SmtpMailer('server','user','password','port');
 //$friendHarvester = new \DiDemo\FriendHarvester( $pdo,$container['mailer']);
-
+/** @var \DiDemo\FriendHarvester $friendHarvester */
 $friendHarvester = $container['friend_harvester'];
 
 $friendHarvester->emailFriends();
---
+++ app/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$container['database.dsn'] =  'sqlite:'.__DIR__.'/../data/database.sqlite';
+$container['smpt.server'] = 'server';
+$container['smpt.user'] = 'user';
+$container['smpt.password'] = 'password';
+$container['smpt.port'] = 'port';
