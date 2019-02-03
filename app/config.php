8de9599f33ecfec4301fd070602ed93611538372
DeuklyoungKo <deutschgon@gmail.com>
2019-02-03 17:05:54 +0100

    for chapter 6-21

--- app/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$container['database.dsn'] =  'sqlite:'.__DIR__.'/../data/database.sqlite';
-$container['smpt.server'] = 'server';
-$container['smpt.user'] = 'user';
-$container['smpt.password'] = 'password';
-$container['smpt.port'] = 'port';
